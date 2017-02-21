3d0a7e296da64e258e87f6681227ffced8c1bee8
Lasse Sprengel <lasse.sprengel@gmail.com>
2017-02-21 19:59:52 +0100

    Added Threaded Job with Queue resolves #6

---
+++ Assets/Scripts/Threading/ThreadedJobWithQueue.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Assets.Scripts.Threading
+{
+    abstract class ThreadedJobWithQueue : ThreadedJob
+    {
+        private Queue<byte[]> m_Queue = new Queue<byte[]>();
+        private bool m_IsCanceled = false;
+        private bool HasUpdate
+        {
+            get
+            {
+                bool tmp;
+                lock (m_Handle)
+                {
+                    tmp = m_Queue.Any();
+                }
+                return tmp;
+            }
+        }
+        protected byte[] Message
+        {
+            private get
+            {
+                byte[] tmp;
+                lock (m_Handle)
+                {
+                    if (!m_Queue.Any())
+                    {
+                        return null; // Should not happen
+                    }
+                    tmp = m_Queue.Dequeue();
+                }
+                return tmp;
+            }
+            set
+            {
+                if (value == null) return;
+                lock (m_Handle)
+                {
+                    m_Queue.Enqueue(value);
+                }
+            }
+        }
+
+        private bool IsCancled
+        {
+            get
+            {
+                bool tmp;
+                lock (m_Handle)
+                {
+                    tmp = m_IsCanceled;
+                }
+                return tmp;
+            }
+            set
+            {
+                lock (m_Handle)
+                {
+                    m_IsCanceled = value;
+                }
+            }
+        }
+
+        protected abstract void ThreadedFunction();
+
+        protected override void ThreadFunction()
+        {
+            while(!IsCancled)
+            {
+                ThreadedFunction();
+            }
+        }
+    }
+}
